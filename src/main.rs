a96746d6f40253667f512784b0a731e4e99f08d9
Hai E <eriran@protonmail.com>
2020-04-22 13:52:38 +0700

    Simple Routing for api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn ob1() -> impl Responder {
+    HttpResponse::Ok().body("General Kenobi!!")
+}
+
+async fn ob2() -> impl Responder {
+    HttpResponse::Ok().body("I have bad feeling about this.")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::scope("/api")
+            .route("/hello/there", web::get().to(ob1))
+            .route("/bad/feeling", web::get().to(ob2))
+        )
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
