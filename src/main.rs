09d60f2dec71e068ef227a0df35d79ae63835f66
Hai E <eriran@protonmail.com>
2020-04-22 16:40:26 +0700

    Working JSON POST Examples

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder, };
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct InputObj {
+struct MyObj {
     name: String,
     number: i32,
+}
+
+/// This handler uses json extractor
+async fn r3ds(item: web::Json<MyObj>) -> HttpResponse {
+    println!("[POST] Value model: {:?}", &item);
+    HttpResponse::Ok().json(&item.0.name) // <- send response
 }
 
 async fn ob1() -> impl Responder {
@@ -15,9 +21,9 @@ async fn ob2() -> impl Responder {
     HttpResponse::Ok().body("I have bad feeling about this.")
 }
 
-async fn r2ds(item: web::Json<InputObj>) -> impl Responder {
+async fn r2ds() -> impl Responder {
     println!("R2DS has been called");
-    HttpResponse::Ok().json(item.0)
+    HttpResponse::Ok().body("Got it")
 }
 
 #[actix_rt::main]
@@ -28,7 +34,7 @@ async fn main() -> std::io::Result<()> {
             .route("/hello/there", web::get().to(ob1))
             .route("/bad/feeling", web::get().to(ob2))
         ).service(web::scope("/app")
-            .route("/may/the/force", web::post().to(r2ds))
+            .route("/may/the/force", web::post().to(r3ds))
         )
     })
     .bind("127.0.0.1:8088")?
