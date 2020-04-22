a3baf7d9ceb7a75c925af87f1fe9d8ad76f77671
Hai E <eriran@protonmail.com>
2020-04-22 16:29:35 +0700

    Attempt to Add json

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
+use serde::{Deserialize, Serialize};
 use warp::Filter;
+
+#[derive(Deserialize, Serialize)]
+struct Items {
+    name: String,
+    rate: u32,
+}
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    pretty_env_logger::init();
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    // POST /employees/:rate  {"name":"Sean","rate":2}
+    let promote = warp::post()
+        .and(warp::path("employees"))
+        .and(warp::path::param::<u32>())
+        // Only accept bodies smaller than 16kb...
+        .and(warp::body::content_length_limit(1024 * 16))
+        .and(warp::body::json())
+        .map(|rate, mut employee: Items| {
+            employee.rate = rate;
+            warp::reply::json(&employee)
+        });
+
+    warp::serve(promote).run(([127, 0, 0, 1], 8088)).await
 }
